8fbcd4a559325dd64d08657286b878cc338683ce
ViktorPavlovA <viktor.pavlov.1998@list.ru>
2023-10-02 21:16:40 +0300

    hot fix

--- Utils/CustomVideoCapture.py
+++ Utils/CustomVideoCapture.py
@@ -19,8 +19,8 @@ class CustomVideoCapture:
         """
         self.cap = cv2.VideoCapture(source)
         self.queue = queue.Queue()
-        self.number_thread = 3
-        self.len_queue = 2
+        self.number_thread = 1
+        self.len_queue = 1
         self.list_thread = []
         self.flag = 0
         self.stop_threads = False
@@ -70,22 +70,22 @@ class CustomVideoCapture:
 Пример использования ниже
 """
 
-# if __name__ == '__main__':
-#     custom_video_capture = CustomVideoCapture(0)
-#     time_start = 0
-#     try:
-#         while True:
-#             success, frame = custom_video_capture.read()
-#             if success:
-#                 fps = 1 / (time.time() - time_start)
-#                 cv2.imshow("frame", frame)
-#                 if cv2.waitKey(1) & 0xFF == ord('q'):
-#                     break
-#                 time_start = time.time()
-#                 print(fps)
-#     except KeyboardInterrupt:  
-#         pass
-#     finally:
-#         custom_video_capture.release()
-#         cv2.destroyAllWindows()
+if __name__ == '__main__':
+    custom_video_capture = CustomVideoCapture(0)
+    time_start = 0
+    try:
+        while True:
+            success, frame = custom_video_capture.read()
+            if success:
+                fps = 1 / (time.time() - time_start)
+                cv2.imshow("frame", frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                time_start = time.time()
+                print(fps)
+    except KeyboardInterrupt:  
+        pass
+    finally:
+        custom_video_capture.release()
+        cv2.destroyAllWindows()
 
